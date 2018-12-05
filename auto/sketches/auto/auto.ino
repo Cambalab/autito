494b3150e319d14ed05d5681e8fbe021418b91b6
Damian Mantuano <smarbos@gmail.com>
2018-12-05 13:59:30 -0300

    [DM] Limpiado y refactorizado autito.ino

--- auto/sketches/auto/auto.ino
+++ auto/sketches/auto/auto.ino
@@ -2,35 +2,28 @@
 #include <AFMotor.h>
 #include <NewPing.h>
 
-int velocidad = 0;
-int sonar_data = 0;
-int masterTiempoActual = 0;
-int masterTiempoInicial = 0;
-int masterTiempoRetardo = 1000;
-int mirandoDerecha = false;
-int accionTiempoActual = 0;
-int accionTiempoInicial = 0;
+int sonar_data = 0; // Esta variable guardara el valor de sensado del ultrasonido.
+int mirandoDerecha = false; // Usamos esta variable para recordar en que direccion giro el robot por ultima vez.
+int sonar_umbral = 15; // Distancia en centimetros
 
-int sonar_umbral = 15; // en centimetros
-
-// Crea el objeto Motor y selecciona el 'puerto' ('port' en inglés) M1, M2, M3 or M4. En este caso, M1
+// Creamos los objetos Motor_izq y Motor_der y selecciona el 'puerto' ('port' en inglés) M1, M2, M3 or M4. En este caso, M1 y M3
 AF_DCMotor motor_izq(1);
 AF_DCMotor motor_der(3);
 NewPing sonar(A0, A1, 200);
 
 void setup() {
   Serial.begin(9600);
-  // Definimos la velocidad para el motor
+  // Definimos la velocidad para los motores
   motor_izq.setSpeed(255);
   motor_der.setSpeed(255);
- // Prendemos el motor
+ // Prendemos los motores
   motor_izq.run(RELEASE);
   motor_der.run(RELEASE);
 }
 
 void girar_izquierda(int delayTime) {
   Serial.println("girar_izquierda");
-  mirandoDerecha = false;
+  mirandoDerecha = false; // Actualizamos la variable para recordar que el robot esta mirando a la izquierda,.
   motor_izq.run(FORWARD);
   motor_der.run(RELEASE);
   delay(delayTime);
@@ -38,7 +31,7 @@ void girar_izquierda(int delayTime) {
 
 void girar_derecha(int delayTime) {
   Serial.println("girar_derecha");
-  mirandoDerecha = true;
+  mirandoDerecha = true; // Actualizamos la variable para recordar que el robot esta mirando a la derecha,.
   motor_izq.run(RELEASE);
   motor_der.run(FORWARD);
   delay(delayTime);
@@ -67,37 +60,42 @@ void parar(int delayTime) {
 
 bool camino_despejado() { // TRUE = camino libre
   Serial.println("camino_despejado");
-    sonar_data = sonar.ping_cm();
-    Serial.println(sonar_data);
-    if (sonar_data > sonar_umbral) {
-      return true;
-    } else {
-      return false;
-    }
+  sonar_data = sonar.ping_cm();
+  Serial.println(sonar_data);
+  if (sonar_data > sonar_umbral) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 void buscar_salida() {
   Serial.println("buscar_salida");
   retroceder(500);
-  girar_izquierda(1000);
+  if (mirandoDerecha) {
+    girar_izquierda(1000);
+  } else {
+    girar_derecha(1000);
+  }
+  parar(1000);
   if (!camino_despejado()) {
     buscar_salida();
   }
 }
 
 void loop() {
-  if(camino_despejado()) {
-      avanzar(500);  
+  if (camino_despejado()) {
+    avanzar(500);
+  } else {
+    parar(1000);
+    retroceder(1000);
+    parar(1000);
+    if (mirandoDerecha) {
+      girar_izquierda(500);
+      parar(1000);
     } else {
+      girar_derecha(500);
       parar(1000);
-      retroceder(1000);
-      parar(1000);
-      if (mirandoDerecha) {
-        girar_izquierda(500);
-        parar(1000);
-      } else {
-        girar_derecha(500);
-        parar(1000);
     }
     parar(2000);
   }
